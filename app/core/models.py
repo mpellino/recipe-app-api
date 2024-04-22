33ea55653f070e19cc8e47c25aa196888a2a15be
mpellino <marcopellino00@gmail.com>
2024-04-22 21:53:17 +0200

    Normalize required email.

--- app/core/models.py
+++ app/core/models.py
@@ -14,7 +14,9 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """create, save and return a new user."""
-        user = self.model(email=email, **extra_fields)
+        if not email:
+            raise ValueError('User must have email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -28,5 +30,5 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=True)
 
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -20,3 +20,30 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test email is normalized for new user"""
+
+        sample_email = [
+            ['test1@EXAMPLE.com', 'test1@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
+            ['TEST3@EXAMPLE.com', 'TEST3@example.com'],
+            ['test4@example.COM', 'test4@example.com']
+        ]
+
+        for email, expected in sample_email:
+            user = get_user_model().objects.create_user(
+                email, 'password123'
+            )
+            self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creting a user without email raises an ValueError"""
+
+        with self.assertRaises(ValueError):
+            password = 'testpass123'
+            email = ''
+            user = get_user_model().objects.create_user(
+                email=email,
+                password=password,
+            )
